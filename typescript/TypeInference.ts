d0f747eefda5e0a09b22f4309cd2656a5567d43f
thzthix <95135403+thzthix@users.noreply.github.com>
2022-02-18 02:16:20 +0900

    Create TypeInference.ts

---
+++ typescript/TypeInference.ts
@@ -0,0 +1,12 @@
+let student={
+    name:'fubao',
+    course:'baby',
+    age:'3',
+    code:function(){
+        console.log('eating bamboo');
+    }
+}
+function calculateCodingIQ(lostPoints){
+    return 100-lostPoints;
+}
+//Type Inference(타입 추론)타입스크립트에서 타입 표기가 없는경우 코드를 읽고 분석하여 타입을 유추
