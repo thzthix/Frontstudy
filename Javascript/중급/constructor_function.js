48571572511f75e0b0b64f45d7405ceb410cd78c
thzthix <95135403+thzthix@users.noreply.github.com>
2022-02-20 02:42:24 +0900

    Create constructor_function.js

---
+++ Javascript/중급/constructor_function.js
@@ -0,0 +1,21 @@
+// 생성자함수:상품 객체를 생성해보자.
+
+function Item(title,price){
+    // this={};
+    
+    this.title=title;
+    this.price=price;
+    this.showPrice=function(){
+        console.log("가격은 ${price}원 입니다");
+    }
+
+    // return this;
+}
+
+const item1=new Item('인형',3000);
+const item2=new Item('가방',4000);
+const item3=new Item('책',5000);
+
+console.log(item1,item2,item3);
+
+item3.showPrice();
