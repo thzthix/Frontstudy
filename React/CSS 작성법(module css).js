131e3af4d3211f86b0fdec7f69f959aca126c0d4
thzthix <95135403+thzthix@users.noreply.github.com>
2022-02-25 03:49:30 +0900

    Create CSS 작성법(module css).js

---
+++ React/CSS 작성법(module css).js
@@ -0,0 +1,25 @@
+
+import styles from "./Hello.module.css"
+export default function Hello(){
+    return (
+    <div>
+    <h1 style={
+  {      
+        color:'#f00',
+        borderRight:'2px solid #000',
+        marginBottom:'30px',
+        opacity:0.5, 
+  }
+    }>Hello</h1>
+    <div className={styles.box}>Hello</div>
+    </div>
+    );
+} 
+/**Hello.module.css
+ * .box {
+  width: 200px;
+  height: 50px;
+  background-color: blue;
+}
+
+ */
