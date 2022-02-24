0d42ada22f38e61ada2812ee02317d6b64925328
thzthix <95135403+thzthix@users.noreply.github.com>
2022-02-25 04:13:41 +0900

    Create  state, useState.js

---
+++ React/state, useState.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Hello from './component/Hello';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Hello />
+      <Hello />
+      <Hello />
+
+    </div>
+  );
+}
+
+export default App;
